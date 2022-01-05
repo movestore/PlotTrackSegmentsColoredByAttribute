7c829c2f26f60cf7627324eccc4bbd439482a53b
Anne Scharf <ascharf@ab.mpg.de>
2022-01-05 12:30:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Plot Of Track Segments Colored By Attribute
+
+MoveApps
+
+Github repository: github.com/movestore/PlotTrackSegmentsColoredByAttribute
+
+## Description
+Each segment of the plotted track(s) can be colored by the values of any of the available attributes associated to the locations. Single or multiple individuals can be displayed simultaneously.
+
+## Documentation
+This App is embedded in an shiny UI, enabling the user to interactively select the attribute by which the segments of the track should be colored, the individuals to be display, the color gradient and if the tracks should be displayed all in one panel, or in multiple panels (one per individual). 
+
+The created plot can than be saved locally via the `Save plot` button. By default the size of the saved plot depends on the screen of the users device. Optionally the plot can be saved with a width and height set by the user. 
+
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+none
+
+The `Save Plot` button enables a local download of the created plot.
+
+### Parameters
+`Select attribute`: one attribute from the drop down list can be selected. All available attributes associated to the locations of the study are displayed.
+
+`Choose display mode`: the tracks can be either displayed on a `Single panel` or on a `Multipanel`, were each individual is displayed on one panel. Currently the plots in the multipanel display have distorted aspect ratio, i.e. it is not 1/1.
+
+`Select colour`: the `low`, `mid` and `high` colors can be chosen to create a customized color scale for continuous attributes. Currently the colors for the display of categorical attributes cannot be customized, default colors will be displayed.
+
+`Select individuals`: one or multiple individuals can be selected.
+
+`Save Plot`: locally downloads the current plot.
+
+`Width(mm)` & `Height(mm)`: optional. Save plot with personalized width and hight in mm.
+
+`Store current configuration`: save the current configuration for future use in the same workflow.
+
+### Null or error handling
+**Data**: For use in further Apps the input data set is returned unmodified. Empty input will give an error.
---
+++ ShinyModule.R
@@ -0,0 +1,129 @@
+library(shiny)
+library(move)
+library(ggplot2)
+library(shinyWidgets)
+library(colourpicker)
+library(htmltools)
+
+## ToDo?: 
+# make it possible to change linesize
+# make zoom-in tool
+# give possibility to select/unselect all individuals with one click
+# give option to only display one individual, ie, when another is selected, the previous one is unselected
+# if variable is factor, give color palette options?
+# make multipanel with aspect ratio==1, look into cowplot pkg
+
+
+shinyModuleUserInterface <- function(id, label) {
+  ns <- NS(id)
+  
+  tagList(
+    titlePanel("Plot track(s) colored by attribute"),
+    fluidRow(
+      column(3,uiOutput(ns('uiAttributeL'))),
+      column(3,selectInput(ns("panels"), "Choose display mode", choices=c("Single panel","Multipanel"), selected="Single panel", multiple=F)),
+      column(2,colourInput(ns("colmin"), "Select colour: low", "blue")),
+      column(2,colourInput(ns("colmid"), "mid", "yellow")),
+      column(2,colourInput(ns("colmax"), "high", "red"))),
+    uiOutput(ns('uiIndivL')),
+    span(textOutput(ns("warningtext")),
+         
+    plotOutput(ns("plot")), style="color:red"),
+    fluidRow(
+      # column(2,numericInput(ns("linesize"), "Width of line in mm", value=0.5, min = 0.1, max = 10, step=0.1)), # does not work for now
+      column(2, style = "margin-top: 25px;", downloadButton(ns('savePlot'), 'Save Plot')),
+      column(3,style = "margin-top: 15px;",helpText("OPTIONAL: save plot with personalized width and height:",style="color:black;font-style: italic"),offset=3),
+      column(2, numericInput(ns("widthmm"), "Width (mm)", value=NA), style = "font-size: 12px; font-style: italic"),
+      column(2, numericInput(ns("heightmm"), "Height (mm)", value=NA),style = "font-size: 12px; font-style: italic"),#
+    )
+  )
+}
+
+shinyModuleConfiguration <- function(id, input) {
+  ns <- NS(id)
+  configuration <- list()
+  configuration
+}
+
+shinyModule <- function(input, output, session, data) {
+  ns <- session$ns
+  current <- reactiveVal(data)
+  output$uiAttributeL <- renderUI({
+   dataCC <- data@data[, colSums(is.na(data@data)) != nrow(data@data)] ## maybe look for a more efective way of doing this in case data set is very large
+    selectInput(ns("attributeL"), "Select attribute", choices=colnames(dataCC))})
+    
+  output$uiIndivL <- renderUI({
+    # checkboxGroupInput(ns("indivL"), "Select individuals", choices=namesIndiv(data), selected=namesIndiv(data)[1], inline=TRUE)
+    checkboxGroupButtons(ns("indivL"), "Select individuals", size="sm", choices=namesIndiv(data), selected=namesIndiv(data)[1],status="default",checkIcon = list(
+      # yes = icon("check-square"), no = icon("square-o")))
+      yes = icon("ok",lib = "glyphicon"))) #, no = icon("remove",lib = "glyphicon")
+  })
+  
+  output$plot <- renderPlot({
+    mDF <- data.frame(long=coordinates(data)[,1],lat=coordinates(data)[,2],attribute=data@data[,input$attributeL], indiv=trackId(data))
+    mDF <- mDF[mDF$indiv %in% c(input$indivL),]
+    if(input$panels=="Single panel"){
+      output$warningtext <- NULL
+      if(is.numeric(mDF[, "attribute"])){ 
+        minattr <- min(mDF[, "attribute"],na.rm=T)
+        maxattr <- max(mDF[, "attribute"],na.rm=T)
+        mpt <- (minattr+maxattr)/2
+        ggplot(mDF) + geom_path(aes(x=long, y=lat, colour=attribute, group=indiv))+ #, size=input$linesize
+          scale_colour_gradient2(low=input$colmin, mid=input$colmid, high=input$colmax, midpoint=mpt, name=input$attributeL
+                                 # , breaks=round(seq(minattr,maxattr,length.out=3),2),labels=round(seq(minattr,maxattr,length.out=3),2)
+          )+
+          coord_fixed()+
+          labs(x ="Longitude", y = "Latitude")+ #title=input$attributeL,
+          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+      }else{ 
+        ggplot(mDF) + geom_path(aes(x=long, y=lat, colour=attribute, group=indiv))+ #, size=input$linesize
+          scale_color_discrete(name=input$attributeL)+
+          coord_fixed()+
+          labs(x ="Longitude", y = "Latitude")+ #title=input$attributeL,
+          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+      }
+    }else if(input$panels=="Multipanel"){
+      output$warningtext <- renderText({"WARNING: Aspect ratio of plots is distorted and not 1/1"})
+      if(nrow(mDF)==0){ ## if plot is empty
+        ggplot(mDF)+labs( x ="Longitude", y = "Latitude")+ #title=input$attributeL,
+          theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+      }else{
+        if(is.numeric(mDF[, "attribute"])){ 
+          minattr <- min(mDF[, "attribute"],na.rm=T)
+          maxattr <- max(mDF[, "attribute"],na.rm=T)
+          mpt <- (minattr+maxattr)/2
+          ggplot(mDF) + geom_path(aes(x=long, y=lat, colour=attribute, group=indiv))+ #, size=input$linesize
+            facet_wrap(~indiv, scales="free")+
+            scale_colour_gradient2(low=input$colmin, mid=input$colmid, high=input$colmax, midpoint=mpt, name=input$attributeL
+                                   # , breaks=round(seq(minattr,maxattr,length.out=3),2),labels=round(seq(minattr,maxattr,length.out=3),2)
+            )+
+            # coord_fixed()+
+            labs( x ="Longitude", y = "Latitude")+ #title=input$attributeL,
+            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                  panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+        }else{ 
+          ggplot(mDF) + geom_path(aes(x=long, y=lat, colour=attribute, group=indiv))+
+            facet_wrap(~indiv, scales="free")+
+            scale_color_discrete(name=input$attributeL)+
+            # coord_fixed()+
+            labs( x ="Longitude", y = "Latitude")+ #title=input$attributeL,
+            theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                  panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+        }
+      }
+    }
+  })     
+
+  ### save plot ###
+  output$savePlot <- downloadHandler(
+    filename = "ColorSegmentsByAttributePlot.png",
+    content = function(file) {
+      ggsave(file, device = "png", units="mm", width = input$widthmm, height = input$heightmm)
+    }
+  )
+  return(reactive({ current() }))
+}
+
